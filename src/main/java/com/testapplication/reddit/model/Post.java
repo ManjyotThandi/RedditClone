b694b3139699442e9e9df4f39582e206f47ea5a9
Manjyot Thandi <manjyot.thandi@gmail.com>
2020-11-19 21:43:12 -0500

    Started post service

--- src/main/java/com/testapplication/reddit/model/Post.java
+++ src/main/java/com/testapplication/reddit/model/Post.java
@@ -1,8 +1,22 @@
 package com.testapplication.reddit.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Post {
 
 	private Long postId; 
 	
 	private String postName;
+	
+	private String url;
+	
+	private String description;
+	
+	private Integer voteCount;
+	
+	
+	
 }
